ecfdc8608bf7d0d2862f46c21ae3f3c6de7bb68a
Linus TOernqvist <linustoernqvist@linuss-mbp.lan>
2022-06-13 15:50:06 -0700

    Navbar and Cover

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { CoverComponent } from './cover/cover.component';
+import { LogoComponent } from './logo/logo.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CoverComponent,
+    LogoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
